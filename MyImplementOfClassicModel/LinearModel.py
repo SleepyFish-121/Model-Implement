ffd5aedfed969f4d4ee9456a7816663cb0ff91f1
Zongxi <yu_zongxi@outlook.com>
2021-05-23 14:16:31 +0800

    Fix error of implementation. Using notebook to test the model and visulize the result.

--- MyImplementOfClassicModel/LinearModel.py
+++ MyImplementOfClassicModel/LinearModel.py
@@ -5,18 +5,13 @@ class LinearRegreesion():
     def __init__(self, ):
         pass
 
-    def fit(self, X: np.array, y: np.array, return_result: bool = True):
+    def fit(self, X: np.array, y: np.array):
         self.X = X
         self.y = y
-        if self.X.shape[0] != self.y.shape[0]:
-            raise Exception("Unmatching X and y")
         self.X = np.hstack([np.array([1] * self.X.shape[0]).reshape(-1, 1), self.X])  # Adding Constant
-        self.b = np.matmul(np.matmul(np.power(np.matmul(self.X.T, self.X), -1), self.X.T), self.y)  # Using OLS
-        if return_result == True:
-            return self, self.b
-        else:
-            return self
+        self.b = np.linalg.inv(self.X.T @ self.X) @ self.X.T @ self.y  # Using OLS
+        return self
 
     def predict(self, X):
-        X = np.hstack([np.array([1]).reshape(1, 1), X])
-        return np.matmul(X, self.b).T
+        X = np.hstack([np.ones((X.shape[0], 1)), X])
+        return X @ self.b
