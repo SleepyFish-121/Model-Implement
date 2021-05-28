181cf6a9367b5f172e0b5883fec50e7cf6eb816a
Zongxi <yu_zongxi@outlook.com>
2021-05-28 12:32:44 +0800

    Working on decision tree classifier (Completed fit() and visualization)

--- DataStructure/BinaryTree.py
+++ DataStructure/BinaryTree.py
@@ -1,3 +1,10 @@
-class BinaryTree(object):
-    def __init__(self, root=None):
+from .Tree import Tree
+
+
+class BinaryTree(Tree) :
+    def __init__(self, root = None, left = None, right = None) :
         self.root = root
+        self.left = left
+        self.right = right
+        Tree.super()
+        del self.children
--- Library/tree/BaseDecisionTree.py
+++ Library/tree/BaseDecisionTree.py
@@ -1,13 +1,11 @@
+from DataStructure import Tree, TreeNode
 from ..base.BaseEstimator import BaseEstimator
-from ..base.BaseEstimator import BaseEstimator
-
 
 class BaseDecisionTree(Tree, BaseEstimator):
     def __init__(self, criterion, max_depth=None):
         self.criterion = criterion
         self.max_depth = max_depth
         super().__init__()
-
     def add_Node(self, input, X, y, func, depth, max_depth):
         if depth == max_depth:
             return TreeNode(value=y)
@@ -25,6 +23,5 @@ class BaseDecisionTree(Tree, BaseEstimator):
                               max_depth))
         value = (input, label)
         return TreeNode(children=children, value=value)
-
     def fit(self, X, y):
         pass
